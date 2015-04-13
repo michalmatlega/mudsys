4af6acc840fc9f828ae132c85638f606f42c3beb
KKuzior <kuziorowiczkrzysztof@gmail.com>
2015-04-14 00:34:23 +0200

    fileman urls fixed

--- mudsys/urls.py
+++ mudsys/urls.py
@@ -8,10 +8,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'mudsys.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^(/)?$', RedirectView.as_view(url='/chats/')),
+    #url(r'^(/)?$', RedirectView.as_view(url='/chats/')),
      #url(r'^(/)?$', include('django.contrib.auth.urls'))
      url(r'^chats/', include('chat.urls', namespace='chat')),
-     url(r'^files/', include('fileman.urls', namespace='fileman')),
+     #url(r'^files/', include('fileman.urls', namespace='fileman')),
      url(r'^admin/', include(admin.site.urls)),
      url(r'^accounts/login/$',  'mudsys.views.login'),
     url(r'^accounts/auth/$',  'mudsys.views.auth_view'),    
