c55eab1e20155a46a8a3a29ba411bd0e504244dc
KKuzior <kuziorowiczkrzysztof@gmail.com>
2015-06-09 22:44:24 +0200

    taskman działa i jest fajny fileman też jest fajny

--- taskman/models.py
+++ taskman/models.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 from django.db import models
-
-
-#class DateTime(models.Model):
-#
-#     datetime = models.DateTimeField(auto_now_add=True)
-
-#    def __unicode__(self):
-#        return unicode(self.datetime)
+from django.core.urlresolvers import reverse
 
 
 class Item(models.Model):
@@ -21,16 +14,3 @@ class Item(models.Model):
 class ItemAdmin(admin.ModelAdmin):
     list_display = ["name", "created", "priority", "difficulty", "done"]
     search_fields = ["name"]
-
-
-#class ItemInLine(admin.TabularInline):
-#    model = Item
-
-
-#class DateAdmin(admin.ModelAdmin):
-#    list_display = ["datetime"]
-#    inlines = [ItemInLine]
-
-
-admin.site.register(Item, ItemAdmin)
-#admin.site.register(DateTime, DateAdmin)
