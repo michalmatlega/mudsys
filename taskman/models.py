c35aa0667a1b1c84ace17ec64b316f87de8b3a9b
KKuzior <kuziorowiczkrzysztof@gmail.com>
2015-06-10 00:16:24 +0200

    rozszerzenie funkcjonalności listy zada

--- taskman/models.py
+++ taskman/models.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
 
 
 class Item(models.Model):
     name = models.CharField(max_length=60)
+    user = models.ForeignKey(User, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
+    desc = models.TextField(max_length=800, default='Description')
     priority = models.IntegerField(default=0)
     difficulty = models.IntegerField(default=0)
     done = models.BooleanField(default=False)
+    progress = models.IntegerField(default=0)
+
+
+    def progress_(self):
+        return "<div style='width: 100px; border: 1px solid #ccc;'>" + \
+               "<div style='height: 4px; width: %dpx; background: #555; '></div></div>" % self.progress
+    progress_.allow_tags = True
 
 
 class ItemAdmin(admin.ModelAdmin):
--- taskman/views.py
+++ taskman/views.py
@@ -2,6 +2,6 @@ from taskman.models import *
 from django.shortcuts import render_to_response
 
 def index(request):
-    item = Item.objects.all()
+    item = Item.objects.order_by('priority')  #[:5] #all()
 
     return render_to_response('tasks/index.html', {'item': item})
